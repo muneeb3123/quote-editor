072c8940f61bc081daa71b07499e343122d7d92a
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-06-25 19:53:33 +0500

    Implemented flash messages with Hotwire in Rails 7 for improved user feedback.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
-    # before_action :authenticate_user!, unless: :devise_controller?
+    before_action :authenticate_user!, unless: :devise_controller?
 
     def current_company
         @current_company ||= current_user.company if user_signed_in?
