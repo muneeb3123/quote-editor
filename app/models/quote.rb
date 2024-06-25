ebe1bbd6576c3dfee99c8aca369b5ae8c244e2ae
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-06-25 17:55:06 +0500

    enhances security by controlling who receives broadcast information, preventing unauthorized access to sensitive data.

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,8 +2,9 @@ class Quote < ApplicationRecord
     # after_create_commit -> { broadcast_prepend_later_to "quotes" }
     # after_update_commit -> { broadcast_replace_later_to "quotes" } 
     # after_destroy_commit -> { broadcast_remove_to "quotes" }
+    belongs_to :company
 
-    broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend # this is equal to above three line
+    broadcasts_to ->(quote) { [ quote.company, "quotes" ] }, inserts_by: :prepend # this is equal to above three line
 
     validates :name, presence: true
     scope :ordered, -> { order(id: :desc) }
