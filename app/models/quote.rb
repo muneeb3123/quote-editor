18f3c81b7c24c887747f483f1cb93e658579dfe8
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-06-25 15:19:35 +0500

    Add real-time updates to Quote model with Turbo Rails

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,11 @@
 class Quote < ApplicationRecord
+    # after_create_commit -> { broadcast_prepend_later_to "quotes" }
+    # after_update_commit -> { broadcast_replace_later_to "quotes" } 
+    # after_destroy_commit -> { broadcast_remove_to "quotes" }
+
+    broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend # this is equal to above three line
+
     validates :name, presence: true
     scope :ordered, -> { order(id: :desc) }
+
   end
