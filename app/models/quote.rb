5eb3dbc2da92763d96706abd73faab39bc7324fb
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-06-26 18:51:34 +0500

    Implement CRUD for dates withTurbo Frames and Streams

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,6 +3,7 @@ class Quote < ApplicationRecord
     # after_update_commit -> { broadcast_replace_later_to "quotes" } 
     # after_destroy_commit -> { broadcast_remove_to "quotes" }
     belongs_to :company
+    has_many :line_item_dates, dependent: :destroy
 
     broadcasts_to ->(quote) { [ quote.company, "quotes" ] }, inserts_by: :prepend # this is equal to above three line
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'pages#home'
-  resources :quotes
+  resources :quotes do
+    resources :line_item_dates, except: [:index, :show]
+  end
 end
