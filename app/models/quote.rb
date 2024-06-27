4641dcd6e6c6c9a4089a2c9c593cbb7dc4b9fdef
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-06-27 15:03:22 +0500

    Adding a quote total with Turbo Frames

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -4,10 +4,15 @@ class Quote < ApplicationRecord
     # after_destroy_commit -> { broadcast_remove_to "quotes" }
     belongs_to :company
     has_many :line_item_dates, dependent: :destroy
+    has_many :line_items, through: :line_item_dates
 
     broadcasts_to ->(quote) { [ quote.company, "quotes" ] }, inserts_by: :prepend # this is equal to above three line
 
     validates :name, presence: true
     scope :ordered, -> { order(id: :desc) }
+
+    def total_price
+      line_items.sum(&:total_price)
+    end
 
   end
